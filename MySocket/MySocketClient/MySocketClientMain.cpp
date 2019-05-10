69f0b20022d6d09066113bf23538f54c7fc60e00
small <513312514@qq.com>
2019-05-10 14:24:49 +0800

    数据结构化传输

--- MySocket/MySocketClient/MySocketClientMain.cpp
+++ MySocket/MySocketClient/MySocketClientMain.cpp
@@ -1,4 +1,8 @@
+/***************************************************
+网络报文的数据结构格式
+***************************************************/
 #define WIN32_LEAN_AND_MEAN
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <string.h>
 
@@ -11,24 +15,47 @@ using namespace std;
 
 enum CMD{
 	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
 	CMD_LOGINOUT,
+	CMD_LOGINOUT_RESULT,
 	CMD_ERROR
 };
 struct DataHeader{
 	short cmd;
 	short dataLength;
 };
-struct Login{
+struct Login : public DataHeader{
+	Login(){
+		cmd = CMD_LOGIN;
+		dataLength = sizeof(Login);
+		memset(userName, 0, sizeof(userName));
+		memset(passWord, 0, sizeof(passWord));
+	}
 	char userName[32];
 	char passWord[32];
 };
-struct LoginResult{
+struct LoginResult : public DataHeader{
+	LoginResult(){
+		cmd = CMD_LOGIN_RESULT;
+		dataLength = sizeof(LoginResult);
+		result = 1;
+	}
 	int result;
 };
-struct Loginout{
+struct Loginout : public DataHeader{
+	Loginout(){
+		cmd = CMD_LOGINOUT;
+		dataLength = sizeof(Loginout);
+		memset(userName, 0, sizeof(userName));
+	}
 	char userName[32];
 };
-struct LoginoutResult{
+struct LoginoutResult : public DataHeader{
+	LoginoutResult(){
+		cmd = CMD_LOGINOUT_RESULT;
+		dataLength = sizeof(LoginoutResult);
+		result = 2;
+	}
 	int result;
 };
 
@@ -67,27 +94,21 @@ int main(){
 			cout << "exit client ..." << endl;
 			break;
 		}else if(strcmp(cmdBuf, "login") == 0){
-			Login login = {"爱白菜", "小昆虫"};
-			DataHeader dh = { CMD_LOGIN, sizeof(Login)};
-			send(cSocket, (const char *)&dh, sizeof(DataHeader), 0);
+			Login login;
+			strcpy(login.userName, "爱白菜");
+			strcpy(login.passWord, "小昆虫");
 			send(cSocket, (const char *)&login, sizeof(Login), 0);
 
-			DataHeader retHeader = {};
-			LoginResult loginRet = {};
-			recv(cSocket, (char *)&retHeader, sizeof(DataHeader), 0);
+			LoginResult loginRet;
 			recv(cSocket, (char *)&loginRet, sizeof(LoginResult), 0);
 
 			cout << "LoginResult: " << loginRet.result << endl;
 		}else if(strcmp(cmdBuf, "logout") == 0){
-			Loginout logout = {"爱白菜的小昆虫"};
-			DataHeader dh = { CMD_LOGINOUT, sizeof(Loginout)};
-			
-			send(cSocket, (const char *)&dh, sizeof(DataHeader), 0);
+			Loginout logout;
+			strcpy(logout.userName, "爱白菜的小昆虫");
 			send(cSocket, (const char *)&logout, sizeof(Loginout), 0);
 
-			DataHeader retHeader = {};
-			LoginoutResult logoutRet = {};
-			recv(cSocket, (char *)&retHeader, sizeof(DataHeader), 0);
+			LoginoutResult logoutRet;
 			recv(cSocket, (char *)&logoutRet, sizeof(LoginResult), 0);
 
 			cout << "LogoutResult: " << logoutRet.result << endl;
--- MySocket/MySocketServer/MySocketServerMain.cpp
+++ MySocket/MySocketServer/MySocketServerMain.cpp
@@ -1,4 +1,8 @@
+/***************************************************
+网络报文的数据结构格式
+***************************************************/
 #define WIN32_LEAN_AND_MEAN
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <string.h>
 
@@ -11,24 +15,47 @@ using namespace std;
 
 enum CMD{
 	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
 	CMD_LOGINOUT,
+	CMD_LOGINOUT_RESULT,
 	CMD_ERROR
 };
 struct DataHeader{
 	short cmd;
 	short dataLength;
 };
-struct Login{
+struct Login : public DataHeader{
+	Login(){
+		cmd = CMD_LOGIN;
+		dataLength = sizeof(Login);
+		memset(userName, 0, sizeof(userName));
+		memset(passWord, 0, sizeof(passWord));
+	}
 	char userName[32];
 	char passWord[32];
 };
-struct LoginResult{
+struct LoginResult : public DataHeader{
+	LoginResult(){
+		cmd = CMD_LOGIN_RESULT;
+		dataLength = sizeof(LoginResult);
+		result = 1;
+	}
 	int result;
 };
-struct Loginout{
+struct Loginout : public DataHeader{
+	Loginout(){
+		cmd = CMD_LOGINOUT;
+		dataLength = sizeof(Loginout);
+		memset(userName, 0, sizeof(userName));
+	}
 	char userName[32];
 };
-struct LoginoutResult{
+struct LoginoutResult : public DataHeader{
+	LoginoutResult(){
+		cmd = CMD_LOGINOUT_RESULT;
+		dataLength = sizeof(LoginoutResult);
+		result = 2;
+	}
 	int result;
 };
 
@@ -85,29 +112,31 @@ int main(){
 		int nLen = recv(cSocket, (char *)&header, sizeof(DataHeader), 0);
 		if(nLen == SOCKET_ERROR){
 			cout << "recv error ..." << endl;
-			return -1;
+			break;
 		}
 		cout << "recv success ..." << endl;
 		cout << "client cmd : " << header.cmd << endl;
 		switch(header.cmd){
 		case CMD_LOGIN:
 			{
-				Login login = {};
-				recv(cSocket, (char *)&login, sizeof(Login), 0);
+				Login login;
+				recv(cSocket, (char *)&login+sizeof(DataHeader), sizeof(Login)-sizeof(DataHeader), 0);
+				cout << "recv cmd : " << login.cmd << endl;
+				cout << "recv len : " << login.dataLength << endl;
 				cout << "userName : " << login.userName << endl;
 				cout << "passWord : " << login.passWord << endl;
-				LoginResult ret = {1};
-				send(cSocket, (const char *)&header, sizeof(DataHeader), 0);
+				LoginResult ret;
 				send(cSocket, (const char *)&ret, sizeof(LoginResult), 0);
 			}
 			break;
 		case CMD_LOGINOUT:
 			{
-				Loginout loginout = {};
-				recv(cSocket, (char *)&loginout, sizeof(Loginout), 0);
+				Loginout loginout;
+				recv(cSocket, (char *)&loginout+sizeof(DataHeader), sizeof(Loginout)-sizeof(DataHeader), 0);
+				cout << "recv cmd : " << loginout.cmd << endl;
+				cout << "recv len : " << loginout.dataLength << endl;
 				cout << "userName : " << loginout.userName << endl;
-				LoginoutResult ret = {2};
-				send(cSocket, (const char *)&header, sizeof(DataHeader), 0);
+				LoginoutResult ret;
 				send(cSocket, (const char *)&ret, sizeof(LoginoutResult), 0);
 			}
 			break;
@@ -120,7 +149,8 @@ int main(){
 			break;
 		}
 	}
-
+	cout << "client exit ..." << endl;
+	closesocket(sSocket);
 	WSACleanup();
 
 	system("pause");
