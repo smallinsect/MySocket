170a3b6e7c88d4214c8178d47632240e7753da49
lianxiang <1161851688@qq.com>
2019-08-06 14:05:26 +0800

    代码修改

--- WebServer/MyServerFile/MyServerWeb.cpp
+++ WebServer/MyServerFile/MyServerWeb.cpp
@@ -3,6 +3,23 @@
 
 
 int request(SOCKET s) {
+	char buf[1024] = "";
+	int iret = recv(s, buf, sizeof(buf), 0);
+	if (iret == SOCKET_ERROR) {
+		printf("[client] recv error ...\n");
+		return -1;
+	}
+	if (iret == 0) {
+		printf("[client] quit ...\n");
+		return 0;
+	}
+	printf("len=%d\n", strlen(buf));
+	printf("******************request header message st******************");
+	printf("%s", buf);
+	printf("******************request header message en******************");
+
+
+	return 1;
 }
 
 void response(SOCKET s) {
--- WebServer/MyServerFile/MyServerWeb.h
+++ WebServer/MyServerFile/MyServerWeb.h
@@ -1,4 +1,6 @@
 #pragma once
+#define 
+
 #include "HeaderFiles.h"
 
 
